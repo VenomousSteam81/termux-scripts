a1bf6d3d53a9803dab88701d69598a2a79e0d417
VenomousSteam81 <jordanjulian.stu@joplinschools.org>
2022-11-04 10:33:54 -0500

    Update ubuntuwithde_novnc.sh

--- ubuntuwithde_novnc.sh
+++ ubuntuwithde_novnc.sh
@@ -28,5 +28,11 @@ echo 'Installing nessecary packages, you might need to hit enter...'
 apt install git curl
 curl -sLf https://raw.githubusercontent.com/Yisus7u7/termux-desktop-xfce/main/boostrap.sh | bash
 git clone https://github.com/noVNC/noVNC
-echo 'To start the server and proxy, cd into noVNC, run vncserver, then ./utils/novnc_proxy --vnc localhost:59xx, where xx is the port number of vncserver'
+echo 'Done... follow next steps'
+
+sleep 1
+clear
+
+echo 'To start the vnc server, run vncserver -listen tcp'
+echo 'To start the proxy, cd into noVNC, run vncserver, then ./utils/novnc_proxy --vnc localhost:59xx, where xx is the port number of vncserver'
 echo 'Make sure to leave issue reports on the GitHub page, https://github.com/VenomousSteam81/termux-scripts, if you run into errors or issues with these scrips!'
