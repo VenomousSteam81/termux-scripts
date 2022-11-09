e712524bd206241f80e152455fcad7912f22ae33
VenomousSteam81 <jordanjulian.stu@joplinschools.org>
2022-11-09 11:56:02 -0600

    Update codeserver_proot.sh

--- codeserver_proot.sh
+++ codeserver_proot.sh
@@ -25,18 +25,19 @@ apt upgrade -y
 apt-get install sudo git -y
 clear
 
-# install nvm for nodejs
+# install nvm for nodejs, and install nodejs v16
 echo "Done with updating and installing packages, now installing nvm..."
 sleep 3
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 echo "You shouldn't have to insert stuff into your .bashrc or etc/profile"
-echo "Exiting..."
-sleep 4
-exit
+export NVM_DIR="$HOME/bash-stuff/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+echo "Now installing NodeJS v16..."
+nvm install v16
 
 # install code-server
-echo "Now, installing code-server..."
+echo "Done! Now installing code-server..."
 sleep 3
-proot-distro login ubuntu
-nvm install v16
-echo "Done installing Node.JS v16, so you should be able to run \'code-server\'
+curl -fsSL https://code-server.dev/install.sh | sh
+echo "Done installing code-server, so you should be able to run code-server
